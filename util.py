7cdc30cc56e38f8af44f24554b7b8ba7bc51d99b
root <root@vagrant.vm>
2018-03-01 21:33:25 +0000

    Extend error handling for commit.

--- util.py
+++ util.py
@@ -4,12 +4,16 @@ from flask import jsonify, make_response
 class Validator():
     def commit(booking):
         mandatory = ['user_id',
+                     'book_date',
                      'amount',
                      'merchant_id',
-                     'ext_ref_nr']
+                     'flight_date',
+                     'flight_origin',
+                     'flight_dest',
+                     'flight_nr']
 
         if not all(key in booking for key in mandatory):
-            return 'Missing one or more mandatory parameters'
+            return 'Missing one or more mandatory parameters for commit service'
 
         return None
 
